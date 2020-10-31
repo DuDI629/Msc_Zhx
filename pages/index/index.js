328f996db18ff9126c0e0c199c6571d8e59a35bc
zenghx <201186668@qq.com>
2020-10-31 22:16:04 +0800

    提交代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,101 +1,73 @@
-//index.js
-//获取应用实例
-const app = getApp()
+// pages/index/index.js
+let app = getApp();
 
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-  motto: 'Welcome My_Msz',
-  userInfo: {},
-  hasUserInfo: false,
-  canIUse: wx.canIUse('button.open-type.getUserInfo'),
-	user_width:'300rpx',
-	Expand:false,
-	Expand1:false,
-	Expand2:false,
+    tabbar: {},
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    app.editTabbar();
   },
-  onReady:function(){
-	 let  that = this;
-	  if(!that.data.hasUserInfo){
-      setTimeout(function(){
-			that.Expand();
-      console.info("你是");
-		},2000);
-	  }
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
   },
-  Expand:function(){
-  	  let that = this;
-  	  that.setData({
-  	  	Expand:true,
-		    Expand1:false,
-        Expand2:false,
-  	  });
-  	  setTimeout(function(){
-  		  that.Expand1();
-      }, 3000);
-  },
-  Expand1:function(){
-	  let that = this;
-	  that.setData({
-	  	Expand1:true,
-	  	Expand:false,
-	  });
-	  setTimeout(function(){
-		  that.Expand2();
-    }, 3000);
-  },
-  Expand2:function(){
-  	  let that = this;
-  	  that.setData({
-  	  	Expand2:true,
-        Expand: false,
-        Expand1: false,
-  	  });
-  	  setTimeout(function(){
-  		  that.Expand();
-      },3000);
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    if (typeof this.getTabBar === 'function' &&
+      this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 0
       })
     }
-	console.info(this.data.hasUserInfo);
-	
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
